5b9d6748cda7f989977b6ee893d48a0d59567038
Uğurcan Çağlayan <ugurcancaglayan@outlook.com>
2021-05-18 18:03:50 +0300

    FluentValidation added.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,16 +23,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        
         public IResult Add(Product product)
-        {
-            //business code
-            //validation
+        {      
+            ValidationTool.Validate(new ProductValidator(), product);
+            //business codes
 
-            if(product.ProductName.Length<2)
-            {
-                //magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
